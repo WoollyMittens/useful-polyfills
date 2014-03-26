c25fd5236a88c69e0bf110b4c0bf05b537847308
Maurice van Creij <maurice.vancreij@webqem.com>
2014-03-26 15:21:26 +1100

    Added override for the console.log replacement.
    When testing on mobile hardware the actual console may not always be
    accessible.

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,8 +82,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-//	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+//	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	// tasts
